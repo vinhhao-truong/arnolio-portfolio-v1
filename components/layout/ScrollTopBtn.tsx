665758d2d8cd74a58450fd9d9b3e0ffb892f5892
vinhhao-truong <vinhhao.truong.52@gmail.com>
2022-10-12 22:26:39 +1100

    Scroll to top works

--- components/layout/ScrollTopBtn.tsx
+++ components/layout/ScrollTopBtn.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from "react";
 import { IoMdArrowDropup } from "react-icons/io";
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 const ScrollTopBtn = () => {
+  const { scrollYProgress } = useScroll();
+
   return (
     <motion.div
       initial={{ y: 20 }}
       animate={{ y: 0 }}
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.8 }}
       className="fixed bottom-[5vh] right-[5vw] cursor-pointer p-2 bg-red-theme rounded-full"
+      onClick={() => {
+        if (window) {
+          if (scrollYProgress.get() > 0) window.scrollTo(0, 0);
+        }
+      }}
     >
       <IoMdArrowDropup className="text-3xl text-white-theme" />
     </motion.div>
