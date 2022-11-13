e4a639fc0f1f81fd0f76bc0aa30ba7f33c00ee0f
vinhhao-truong <vinhhao.truong.52@gmail.com>
2022-11-13 20:47:37 +1100

    Animating cards

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -4,7 +4,7 @@ import SectionHeader from "../SectionHeader";
 import Container from "../Container";
 import Section from "../Section";
 import { motion } from "framer-motion";
-import { getClasses } from "../../utils/getProps";
+import { getClasses, getStyles } from "../../utils/getProps";
 import getRandomNum from "../../utils/getRandomNum";
 
 interface card {
@@ -12,34 +12,35 @@ interface card {
   content: React.ReactNode;
   description?: string;
   className?: string;
+  coord: number[];
 }
 const cardList: card[] = [
   {
-    title: "Bio",
+    title: "About me",
     content: <div className=""></div>,
+    coord: [-10, 30],
   },
   {
-    title: "Bio",
+    title: "Tech stacks",
     content: <div className=""></div>,
+    coord: [50, 0],
   },
   {
-    title: "Bio",
+    title: "Experience",
     content: <div className=""></div>,
-  },
-  {
-    title: "Bio",
-    content: <div className=""></div>,
+    coord: [50, 50],
   },
 ];
 
 const colorList: string[] = [
-  "bg-blue-300",
+  "bg-sky-300",
   "bg-indigo-300",
   "bg-rose-300",
   "bg-green-300",
   "bg-lime-300",
   "bg-red-300",
   "bg-violet-300",
+  "bg-blue-300",
   "bg-yellow-300",
   "bg-orange-300",
   "bg-pink-300",
@@ -48,35 +49,46 @@ const colorList: string[] = [
   "bg-emerald-300",
   "bg-teal-300",
   "bg-cyan-300",
-  "bg-sky-300",
 ];
 
 interface AboutProps extends ReactProps {}
 const About: React.FC<AboutProps> = ({}) => {
   const cardContainerRef = useRef(null);
 
-  const [currentCard, setCurrentCard] = useState<number>(cardList.length - 1);
+  const [currentCard, setCurrentCard] = useState<number>(0);
 
   interface CardProps extends ReactProps {
-    rotate: number;
     onClick?: React.MouseEventHandler<HTMLDivElement>;
+    isOnTop: boolean;
+    coord: number[];
   }
   const Card: React.FC<CardProps> = ({
     children,
     className,
-    rotate,
+    style,
     onClick,
+    isOnTop,
+    coord,
   }) => {
+    const rotate: number = getRandomNum(-15, 15);
+
     return (
       <motion.div
         className={`absolute p-3 ${getClasses(
           className
-        )} rounded-lg shadow-lg lg:w-full lg:h-full cursor-grab`}
-        drag
-        dragConstraints={cardContainerRef}
-        whileDrag={{ cursor: "grabbing", rotate: 0, zIndex: 1 }}
-        animate={{ rotate: rotate }}
+        )} rounded-lg shadow-lg lg:w-[15rem] lg:h-[25rem] cursor-grab`}
+        // exit={{ zIndex: 1 }}
+        whileHover={{ scale: 1.02 }}
+        animate={{
+          rotate: isOnTop ? 0 : rotate,
+          top: isOnTop ? "50%" : `${coord[0]}%`,
+          left: isOnTop ? "50%" : `${coord[1]}%`,
+          x: isOnTop ? "-50%" : 0,
+          y: isOnTop ? "-50%" : 0,
+        }}
+        style={{ ...getStyles(style) }}
         onClick={onClick}
+        // onDragExit={() => {}}
       >
         <div className="block mx-auto my-3 bg-transparent rounded-full lg:w-4 lg:h-4"></div>
         {children}
@@ -91,26 +103,23 @@ const About: React.FC<AboutProps> = ({}) => {
   return (
     <Section id="about" className="relative">
       <SectionHeader title="About" />
-      <Container className="h-[80vh] lg:grid lg:grid-cols-5">
+      <Container className="h-[80vh] lg:grid lg:grid-cols-2">
         {/* LEFT */}
         <div
-          className="relative flex items-center justify-center bg-blue-200 lg:col-span-2 lg:h-full"
+          className="relative flex items-center justify-center bg-blue-400 lg:h-full"
           ref={cardContainerRef}
         >
-          <div className="lg:relative lg:w-3/5 max-h-[40rem] max-w-[30rem] lg:h-4/6">
-            {cardList.map(({ className, title }, idx) => {
-              const deg: number = getRandomNum(-18, 18);
-              const isCurrent: boolean = idx === currentCard;
-              // (getRandomNum(0, cardList.length - 1) % 4);
+          <div className="lg:relative lg:w-4/5 max-h-[40rem] max-w-[30rem] lg:h-4/5 bg-green-300">
+            {cardList.map(({ className, title, coord }, idx) => {
+              const isOnTop: boolean = currentCard === idx;
 
               return (
                 <Card
-                  className={`${getClasses(className)} ${colorList[idx]} ${
-                    isCurrent ? "z-[1]" : "z-0"
-                  }`}
+                  className={`${getClasses(className)} ${colorList[idx]}`}
                   key={idx}
-                  rotate={deg}
+                  coord={coord}
                   onClick={() => setCurrentCard(idx)}
+                  isOnTop={isOnTop}
                 >
                   {title}
                 </Card>
